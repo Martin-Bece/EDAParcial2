ef658ddc8dd6587b028d365a6d1aa95b60caf5c3
Martin-Bece <martinbecerrasl7@gmail.com>
2025-06-03 18:43:39 -0300

    Clase Nodo, nada mas

---
+++ EDAParcial2/src/edaparcial2/Nodo.java
@@ -0,0 +1,14 @@
+package edaparcial2;
+
+public class Nodo {
+    String palabra;
+    int contador;
+    Nodo izq, der;
+    
+    public Nodo(String palabra){
+        this.palabra = palabra;
+        this.contador = contador;
+        izq = null;
+        der = null;
+    }
+}
