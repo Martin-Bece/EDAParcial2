28637dd0e9172f53f90791670b51ede299ac74b0
Marianela <rocamoramarianela86@gmail.com>
2025-06-05 19:57:36 -0300

    menu

--- EDAParcial2/src/edaparcial2/EDAParcial2.java
+++ EDAParcial2/src/edaparcial2/EDAParcial2.java
@@ -1,10 +1,47 @@
 package edaparcial2;
 
-public class EDAParcial2 {
+import java.util.Scanner;
 
+public class EDAParcial2 {
+    
     public static void main(String[] args) {
         Arbol a = new Arbol();
-        
+         
+        String opcion;
+        Scanner leer=new Scanner (System.in);
+
+        do {
+            System.out.println("\n--- Menú de Análisis de Discurso ---");
+            System.out.println("1. Ingresar un texto/discurso");
+            System.out.println("2. Saber la cantidad de palabras usadas");
+            System.out.println("3. Saber la palabra que más se repite (omitiendo artículos y monosílabos)");
+            System.out.println("4. Saber cuántas palabras negativas o poco inteligentes se usan");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = leer.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    //ingresarTexto();
+                    break;
+                case "2":
+                    //saberCantidadPalabrasUsadas();
+                    break;
+                case "3":
+                    //saberPalabraMasRepetida();
+                    break;
+                case "4":
+                    //saberCuantasPalabrasNegativasSeUsan();
+                    break;        
+                case "5":
+                    System.out.println("Saliendo de la aplicación");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, intente de nuevo.");
+            }
+        } while (!opcion.equals("5"));
+        leer.close();
+    
 //        a.insertar("Pancreas");
 //        a.insertar("Las");
 //        a.insertar("La");
@@ -28,6 +65,8 @@ public class EDAParcial2 {
         
         a.inOrden();
         
+       
     }
     
+
 }
